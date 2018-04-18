02adeb17334f80645d968c416996a0cf73b66f7c
suisun <suisun@dummy.com>
2018-04-18 22:49:42 +0800

    Init

--- f_server.js
+++ f_server.js
@@ -29,6 +29,42 @@ isEmpty = function(obj) {
     return true;
 }
 
+upload = function(req,res,dir) {
+    console.log(req.query);
+
+    var guid = req.query.guid;
+  
+    if (validate(guid) == false) {
+        guid = "none";
+    }
+    console.log( guid );
+    var outfilename = "";
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+    // store all uploads in the /uploads directory
+//        form.uploadDir = path.join(__dirname, '/uploads');
+    form.uploadDir = "client/"+dir;
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function(field, file) {
+        outfilename = guid_Date.now() + '_' + file.name;
+        fs.rename(file.path, path.join(form.uploadDir, outfilename));
+    });
+    // log any errors that occur
+    form.on('error', function(err) {
+        console.log('An error has occured: \n' + err);
+        res.json({success:false,error:'fail'});
+    });
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function() {
+        res.json({success:true,data: { filename:outfilename}});
+    });
+    // parse the incoming request containing the form data
+    form.parse(req);
+};
+
 //-------------------------------------------------------//
 //-------------------------------------------------------//
 //-------------------------------------------------------//
@@ -36,14 +72,35 @@ app.get('/', function(req, res) {
     console.log( "/"+ JSON.stringify(req.params) );
     res.render('index.html');
 });
+
 //-------------------------------------------------------//
-app.get('/photo/:page', function(req, res) {
+app.get('/avatar/:page', function(req, res) {
     console.log( JSON.stringify(req.params) );
     var filename = req.params.page;
     if( filename == null ) {
         return;
     }
-    filename = './client/photos/' + req.params.page;
+    filename = './client/avatar/' + req.params.page;
+    if( !fs.existsSync(filename) ) { 
+        res.send('done');
+        return;
+    } 
+    console.log( filename );
+    try {
+        res.sendfile(filename);
+    } catch (e) {
+        console.log('         error loading page-' + req.params.page );
+        res.send('done');
+    }
+});
+
+app.get('/image/:page', function(req, res) {
+    console.log( JSON.stringify(req.params) );
+    var filename = req.params.page;
+    if( filename == null ) {
+        return;
+    }
+    filename = './client/image/' + req.params.page;
     if( !fs.existsSync(filename) ) { 
         res.send('done');
         return;
@@ -59,41 +116,12 @@ app.get('/photo/:page', function(req, res) {
 
 //----------------------------------------------------------------------//
 // upload
-app.post('/upload', function(req, res) {
+app.post('/upload_avatar', function(req, res) {
     console.log(req.query);
+    upload(req,res,"avatar")
+});
 
-    var guid = req.query.guid;
-  
-    if (validate(guid) == false) {
-        guid = "";
-    }
-    console.log( guid );
-    var outfilename = "";
-    // create an incoming form object
-    var form = new formidable.IncomingForm();
-    // specify that we want to allow the user to upload multiple files in a single request
-    form.multiples = true;
-    // store all uploads in the /uploads directory
-//        form.uploadDir = path.join(__dirname, '/uploads');
-    form.uploadDir = 'client/photos';
-    // every time a file has been uploaded successfully,
-    // rename it to it's orignal name
-    form.on('file', function(field, file) {
-        console.log( field );
-        console.log( file );
-        outfilename = Date.now() + '_' + file.name;
-        console.log( outfilename );
-        fs.rename(file.path, path.join(form.uploadDir, outfilename));
-    });
-    // log any errors that occur
-    form.on('error', function(err) {
-        console.log('An error has occured: \n' + err);
-        res.json({success:false,error:'fail'});
-    });
-    // once all the files have been uploaded, send a response to the client
-    form.on('end', function() {
-        res.json({success:true,data: { filename:outfilename}});
-    });
-    // parse the incoming request containing the form data
-    form.parse(req);
+app.post('/upload_image', function(req, res) {
+    console.log(req.query);
+    upload(req,res,"image")
 });
